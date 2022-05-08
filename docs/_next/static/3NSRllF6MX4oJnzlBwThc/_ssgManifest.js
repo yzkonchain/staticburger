2ab6caa8201bec1cba891d21af84c54e77289a5d
yuzhoukai <yuzhoukai@onchain.com>
2022-05-08 11:28:58 +0800

    docs

---
+++ docs/_next/static/3NSRllF6MX4oJnzlBwThc/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[lang]\u002Fgovernance","\u002F[lang]\u002Fgovernance\u002Fdetail","\u002F[lang]\u002Fairdrop","\u002F[lang]\u002Fhome","\u002F[lang]\u002Ftreasury","\u002F[lang]\u002Fdashboard"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
